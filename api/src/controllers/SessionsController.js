3475162a2ba5e2b115f8d28fc8445344366a8f62
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-10-31 23:03:20 -0300

    commit

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -19,13 +19,15 @@ class SessionsController {
     if (!passwordMatched) {
       throw new AppError("E-mail e/ou senha incorreta.", 401);
     }
-                    
+
     const { secret, expiresIn } = authConfig.jwt;
 
     const token = sign({ role: user.role }, secret, {
       subject: String(user.id),
       expiresIn
     });
+
+    delete user.password
 
     response.status(201).json({ token, user });
   }
